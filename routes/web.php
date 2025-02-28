26d2dd29e2691f24480c8f5409a2c79f487885e7
Your Name <you@example.com>
2025-02-28 15:06:37 +0700

    update3

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/about', function () {
     return view('about', ['nama' => 'Apris Agung48']);
 });
@@ -13,4 +13,7 @@ Route::get('/home', function () {
 });
 Route::get('/blog', function () {
     return view('blog');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
