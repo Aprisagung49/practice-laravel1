48d2c05571c066d438c65fb9b0676031db444f54
Your Name <you@example.com>
2025-03-03 12:00:41 +0700

    models

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:title>{{$title}}</x-slot:title>
+    <h3 class="text-2xl text-gray-900 font-bold">Blog Spot Today</h3>
     @foreach ($posts as $post )
-    <h3>Blog Spot</h3>
     <article class ="py-8 max-w-screen-md border-b border-gray-300">
         <a href="/posts/{{$post['slug']}}" class="hover:underline">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{$post['title']}}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
+
 Route::get('/about', function () {
     return view('about', ['nama' => 'Apris Agung48'], ['title' => 'About Page']);
 });
@@ -13,48 +17,14 @@ Route::get('/home', function () {
     return view('home', ['title' => 'HomePage' ]);
 });
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog Page', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'perkembangan-era-digitalisasi',
-            'title' => 'Perkembangan Era Digitalisasi',
-            'author' => 'Apris Agung Wiresa',
-            'body' => 'Perkembangan era digital juga semakin lama semakin berjalan begitu cepat hingga tak bisa dihentikan oleh manusia. Hal ini tak lain karena kita sebagai manusia sendirilah yang pada akhirnya menuntut dan meminta berbagai macam hal secara lebih efisien serta praktis.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'Era-Digital-40',
-            'title' => 'Digitalisasi 4.0',
-            'author' => 'Apris Agung Wiresa',
-            'body' => 'Salah satu pihak yang paling menggiatkan transformasi digital adalah industri atau perusahaan. Mengingat pentingnya peran sektor industri dalam pengembangan ekonomi dan teknologi suatu negeri, hal ini tentu dianggap sebagai sesuatu yang wajar dan masuk akal. Transformasi digital untuk perusahaan adalah hal yang tak bisa dielakkan, terutama setelah pengembangan konsep Industri 4.0 yang isunya mulai diangkat sejak satu dekade silam.'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
 });
 route::get('/posts/{slug}', function ($slug){
     // dd($id); //dd digunakan untuk debug atau var_dump 
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'perkembangan-era-digitalisasi',
-            'title' => 'Perkembangan Era Digitalisasi',
-            'author' => 'Apris Agung Wiresa',
-            'body' => 'Perkembangan era digital juga semakin lama semakin berjalan begitu cepat hingga tak bisa dihentikan oleh manusia. Hal ini tak lain karena kita sebagai manusia sendirilah yang pada akhirnya menuntut dan meminta berbagai macam hal secara lebih efisien serta praktis.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'Era-Digital-40',
-            'title' => 'Digitalisasi 4.0',
-            'author' => 'Apris Agung Wiresa',
-            'body' => 'Salah satu pihak yang paling menggiatkan transformasi digital adalah industri atau perusahaan. Mengingat pentingnya peran sektor industri dalam pengembangan ekonomi dan teknologi suatu negeri, hal ini tentu dianggap sebagai sesuatu yang wajar dan masuk akal. Transformasi digital untuk perusahaan adalah hal yang tak bisa dielakkan, terutama setelah pengembangan konsep Industri 4.0 yang isunya mulai diangkat sejak satu dekade silam.'
-        ]
-        ];
-
-        $post = Arr::first($posts, function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
+        $post = Post::find($slug);
 
         return view ('post',['title' => 'Single Post', 'post'=>$post]);
-});
+}); 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact US']);
 });
