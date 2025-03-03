6aee2b8abd89b87de49a95bb9900b3fc20521374
Your Name <you@example.com>
2025-03-03 11:29:05 +0700

    view-data-1

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog Page', 'posts' => [
         [
             'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Arikel 1',
+            'slug' => 'perkembangan-era-digitalisasi',
+            'title' => 'Perkembangan Era Digitalisasi',
             'author' => 'Apris Agung Wiresa',
             'body' => 'Perkembangan era digital juga semakin lama semakin berjalan begitu cepat hingga tak bisa dihentikan oleh manusia. Hal ini tak lain karena kita sebagai manusia sendirilah yang pada akhirnya menuntut dan meminta berbagai macam hal secara lebih efisien serta praktis.'
         ],
         [
             'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Arikel 2',
+            'slug' => 'Era-Digital-40',
+            'title' => 'Digitalisasi 4.0',
             'author' => 'Apris Agung Wiresa',
             'body' => 'Salah satu pihak yang paling menggiatkan transformasi digital adalah industri atau perusahaan. Mengingat pentingnya peran sektor industri dalam pengembangan ekonomi dan teknologi suatu negeri, hal ini tentu dianggap sebagai sesuatu yang wajar dan masuk akal. Transformasi digital untuk perusahaan adalah hal yang tak bisa dielakkan, terutama setelah pengembangan konsep Industri 4.0 yang isunya mulai diangkat sejak satu dekade silam.'
         ]
@@ -35,15 +35,15 @@ route::get('/posts/{slug}', function ($slug){
     $posts = [
         [
             'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Arikel 1',
+            'slug' => 'perkembangan-era-digitalisasi',
+            'title' => 'Perkembangan Era Digitalisasi',
             'author' => 'Apris Agung Wiresa',
             'body' => 'Perkembangan era digital juga semakin lama semakin berjalan begitu cepat hingga tak bisa dihentikan oleh manusia. Hal ini tak lain karena kita sebagai manusia sendirilah yang pada akhirnya menuntut dan meminta berbagai macam hal secara lebih efisien serta praktis.'
         ],
         [
             'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Arikel 2',
+            'slug' => 'Era-Digital-40',
+            'title' => 'Digitalisasi 4.0',
             'author' => 'Apris Agung Wiresa',
             'body' => 'Salah satu pihak yang paling menggiatkan transformasi digital adalah industri atau perusahaan. Mengingat pentingnya peran sektor industri dalam pengembangan ekonomi dan teknologi suatu negeri, hal ini tentu dianggap sebagai sesuatu yang wajar dan masuk akal. Transformasi digital untuk perusahaan adalah hal yang tak bisa dielakkan, terutama setelah pengembangan konsep Industri 4.0 yang isunya mulai diangkat sejak satu dekade silam.'
         ]
