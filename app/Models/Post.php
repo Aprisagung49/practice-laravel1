2dd8ab9e4b9aadb2f1e998936fdda876cc516b9a
Your Name <you@example.com>
2025-03-04 10:31:57 +0700

    Relasi-db

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
@@ -10,4 +11,12 @@ class Post extends Model
 
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+// function ini digunakan untuk agar author dapat mengambil atau relasi dengan 
+// tabel user jadi author atau disini table post dapat mengambil
+// data dari tabel user Atau ONE To Many
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -22,6 +23,11 @@ Route::get('/posts', function () {
 route::get('/posts/{post:slug}', function (Post $post){ //Post disini adalah nama model kita
     // dd($id); //dd digunakan untuk debug atau var_dump 
         return view ('post',['title' => 'Single Post', 'post'=>$post]);
+}); 
+route::get('/authors/{user}', function (User $user){ //Post disini adalah nama model kita
+    // dd($id); //dd digunakan untuk debug atau var_dump 
+        return view ('posts',['title' => 'Artikel By : ' . $user->name, 'posts'=>
+        $user->posts]);
 }); 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact US']);
